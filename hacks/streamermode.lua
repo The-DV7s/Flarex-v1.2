0934330d7e48287b890db76cce8d3e484a37ee92
Flareon <167229955+xyzkyle@users.noreply.github.com>
2024-05-04 21:52:17 +0500

    Create streamermode.lua

---
+++ hacks/streamermode.lua
@@ -0,0 +1,19 @@
+-- why does this file use lua? to CHANGE NAME
+
+return(
+  function
+    ([[Obfuscated With FlarexSec:v0.1]])
+    local E = [["This obfuscator is in progress... SO This might be easy to decrypt"]]
+  function{return(function(...e) if a == "NOT DENIFIED" then function(game:setName);(game(#^etdvt3WBc8888**(e()); smegXEFX('Bytes have been hidden',local df=[[Default="Flrx User"]]);else(;function(gameguards(antife)));
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+local fe = {game:LocalStorage(Nickname:"Player",smegXEFX(df))}; (function(setmetatable)(...)( u:function.. true? e'else no'; e)  "{Mangled Shuffled=true}" = r lacol; "betataest"[[ toats]] ;;;; (https://flare.x/)
